4c3b680a1455a6bd200a5f9d1995238e6cc454ed
Gabrielbemo <gabrielbemoura@gmail.com>
2018-08-09 14:47:22 -0300

    Ajustando pontos de habilidades

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,13 +1,20 @@
 module.exports.jogo = function (application, req, res) {
-    if (req.session.autorizado) {
-        res.render('jogo');
-    }else{
+    if (req.session.autorizado !== true) {
         res.send('Usuário precisa fazer login');
     }
+
+    var usuario = req.session.usuario;
+    var casa = req.session.casa;
+
+    var connection = application.config.dbConnection;
+    var JogoDAO = new application.app.models.JogoDAO(connection);
+
+    JogoDAO.iniciaJogo(res,usuario,casa);
+    
 }
 
 module.exports.sair = function (application, req, res) {
-    req.session.destroy(function(err){
-        res.render("index", {validacao: {}});
+    req.session.destroy(function (err) {
+        res.render("index", { validacao: {} });
     });
 }
