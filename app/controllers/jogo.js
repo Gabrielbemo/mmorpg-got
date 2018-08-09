da87fcf7a6a4b9ef930bf0cfa6d4ca2e438d1822
Gabrielbemo <gabrielbemoura@gmail.com>
2018-08-09 15:17:41 -0300

    Carregando as views de súditos e pergaminhos via Ajax com JQuery

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -9,12 +9,24 @@ module.exports.jogo = function (application, req, res) {
     var connection = application.config.dbConnection;
     var JogoDAO = new application.app.models.JogoDAO(connection);
 
-    JogoDAO.iniciaJogo(res,usuario,casa);
-    
+    JogoDAO.iniciaJogo(res, usuario, casa);
+
 }
 
 module.exports.sair = function (application, req, res) {
     req.session.destroy(function (err) {
         res.render("index", { validacao: {} });
     });
+}
+
+module.exports.suditos = function (application, req, res) {
+
+    res.render("aldeoes", { validacao: {} });
+
+}
+
+module.exports.pergaminhos = function (application, req, res) {
+
+    res.render("pergaminhos", { validacao: {} });
+
 }
