cbca1ea4521cfc5fedde35344aa28fd9fe5176ff
Gabrielbemo <gabrielbemoura@gmail.com>
2018-08-10 09:56:17 -0300

    gravando ações no banco de dados

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -6,10 +6,15 @@ module.exports.jogo = function (application, req, res) {
     var usuario = req.session.usuario;
     var casa = req.session.casa;
 
+    var msg = '';
+    if (req.query.msg != '') {
+        msg = req.query.msg;
+    }
+
     var connection = application.config.dbConnection;
     var JogoDAO = new application.app.models.JogoDAO(connection);
 
-    JogoDAO.iniciaJogo(res, usuario, casa);
+    JogoDAO.iniciaJogo(res, usuario, casa, msg);
 
 }
 
@@ -20,13 +25,42 @@ module.exports.sair = function (application, req, res) {
 }
 
 module.exports.suditos = function (application, req, res) {
-
+    if (req.session.autorizado !== true) {
+        res.send('Usuário precisa fazer login');
+    }
     res.render("aldeoes", { validacao: {} });
 
 }
 
 module.exports.pergaminhos = function (application, req, res) {
-
+    if (req.session.autorizado !== true) {
+        res.send('Usuário precisa fazer login');
+    }
     res.render("pergaminhos", { validacao: {} });
+
+}
+
+module.exports.ordenar_acao_sudito = function (application, req, res) {
+    if (req.session.autorizado !== true) {
+        res.send('Usuário precisa fazer login');
+    }
+    var dadosForm = req.body;
+
+    req.assert('acao', 'Ação deve ser informada').notEmpty();
+    req.assert('quantidade', 'Quantidade deve ser informada').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if (erros) {
+        res.redirect('jogo?msg=A');
+        return;
+    }
+
+    var connection = application.config.dbConnection;
+    var JogoDAO = new application.app.models.JogoDAO(connection);
+    dadosForm.usuario = req.session.usuario;
+    JogoDAO.acao(dadosForm);
+
+    res.redirect('jogo?msg=B');
 
 }
