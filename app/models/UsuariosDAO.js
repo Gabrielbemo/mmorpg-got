0ff52545d7e50cf6802ebc2204a2f9aa6dc533aa
Gabrielbemo <gabrielbemoura@gmail.com>
2018-08-16 22:36:58 -0300

    adicionado o módulo crypto para as senhas

--- app/models/UsuariosDAO.js
+++ app/models/UsuariosDAO.js
@@ -1,32 +1,42 @@
-function UsuariosDAO(connection){
-	
+/* importar o  ódulo do crypto */
+var crypto = require('crypto');
+
+function UsuariosDAO(connection) {
+
 	this._connection = connection();
 }
 
-UsuariosDAO.prototype.inserirUsuario = function(usuario){
-	this._connection.open( function(err, mongoclient){
-		mongoclient.collection("usuarios", function(err, collection){
-			collection.insert(usuario);
+UsuariosDAO.prototype.inserirUsuario = function (usuario) {
+	this._connection.open(function (err, mongoclient) {
+		mongoclient.collection("usuarios", function (err, collection) {
 
+			var senha_criptografada = crypto.createHash('md5').update(usuario.senha).digest('hex');
+
+			usuario.senha = senha_criptografada;
+
+			collection.insert(usuario);
 			mongoclient.close();
 		});
 	});
 }
 
-UsuariosDAO.prototype.autenticar = function(usuario,req,res){
-	this._connection.open( function(err, mongoclient){
-		mongoclient.collection("usuarios", function(err, collection){
-			collection.find(usuario).toArray(function(err, result){
-			
-				if(result[0] != undefined){
+UsuariosDAO.prototype.autenticar = function (usuario, req, res) {
+	this._connection.open(function (err, mongoclient) {
+		mongoclient.collection("usuarios", function (err, collection) {
+			var senha_criptografada = crypto.createHash('md5').update(usuario.senha).digest('hex');
+
+			usuario.senha = senha_criptografada;
+			collection.find(usuario).toArray(function (err, result) {
+
+				if (result[0] != undefined) {
 					req.session.autorizado = true;
 					req.session.usuario = result[0].usuario;
 					req.session.casa = result[0].casa;
 				}
-				if(req.session.autorizado){
+				if (req.session.autorizado) {
 					res.redirect("jogo");
-				}else{
-					res.render("index",{validacao: {}});
+				} else {
+					res.render("index", { validacao: {} });
 				}
 			});
 			mongoclient.close();
@@ -34,6 +44,6 @@ UsuariosDAO.prototype.autenticar = function(usuario,req,res){
 	});
 }
 
-module.exports = function(){
+module.exports = function () {
 	return UsuariosDAO;
 }
